764c79a1eb8cafe79200cfd664cd2139ec6146f0
Simon Ostendorf <github@simon-ostendorf.de>
2023-10-24 18:07:06 +0200

    fix(config/database): redis cluster (#83)

--- config/database.php
+++ config/database.php
@@ -129,21 +129,25 @@ return [
 
         'clusters' => [
             'default' => [
-                'host' => env('REDIS_HOST', '127.0.0.1'),
-                'username' => env('REDIS_USERNAME'),
-                'password' => env('REDIS_PASSWORD', null),
-                'port' => env('REDIS_PORT', '6379'),
-                'database' => env('REDIS_DB', '0'),
-                'prefix' => '',
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
+                    'prefix' => '',
+                ],
             ],
 
             'cache' => [
-                'host' => env('REDIS_HOST', '127.0.0.1'),
-                'username' => env('REDIS_USERNAME'),
-                'password' => env('REDIS_PASSWORD', null),
-                'port' => env('REDIS_PORT', '6379'),
-                'database' => env('REDIS_CACHE_DB', '1'),
-                'prefix' => 'cache',
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_CACHE_DB', '1'),
+                    'prefix' => 'cache',
+                ],
             ],
         ],
     ],
