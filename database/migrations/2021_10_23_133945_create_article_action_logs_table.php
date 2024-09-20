51fa7b781c81e3f95d32212fe931057852aec934
Shift <shift@laravelshift.com>
2024-09-20 12:57:22 +0000

    Adopt anonymous migrations

--- database/migrations/2021_10_23_133945_create_article_action_logs_table.php
+++ database/migrations/2021_10_23_133945_create_article_action_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticleActionLogsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class CreateArticleActionLogsTable extends Migration
     {
         Schema::dropIfExists('article_action_logs');
     }
-}
+};
--- database/migrations/2023_10_20_163214_update_img_in_persons_table.php
+++ database/migrations/2023_10_20_163214_update_img_in_persons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateImgInPersonsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class UpdateImgInPersonsTable extends Migration
             $table->string('img', 100)->nullable(false)->change();
         });
     }
-}
+};
