fd0079ff77964996510cb4fbc4c9de42d84a0ff7
Shift <shift@laravelshift.com>
2024-10-06 17:19:53 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,19 +7,5 @@ return [
 
     'public_url' => env('APP_PUBLIC_URL', 'http://localhost'),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
 ];
