b261278adc7fbf3836e3d904ac852ff6a45ae94f
Shift <shift@laravelshift.com>
2024-10-06 08:45:05 +0000

    Shift core files

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->configureRateLimiting();
+        // RateLimiter::for('api', function (Request $request) {
+        //     return Limit::perMinute(1200)->by(optional($request->user())->id ?: $request->ip());
+        // });
 
         $this->routes(function () {
             Route::prefix('api')
@@ -36,17 +38,5 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
-    }
-
-    /**
-     * Configure the rate limiters for the application.
-     *
-     * @return void
-     */
-    protected function configureRateLimiting()
-    {
-        // RateLimiter::for('api', function (Request $request) {
-        //     return Limit::perMinute(1200)->by(optional($request->user())->id ?: $request->ip());
-        // });
     }
 }
