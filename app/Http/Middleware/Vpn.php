1779747f46a08bcb1fa563b84465642b51b1af63
Simon Ostendorf <github@simon-ostendorf.de>
2024-10-25 16:41:38 +0200

    feat: readd APP_VPN_IP

--- app/Http/Middleware/Vpn.php
+++ app/Http/Middleware/Vpn.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use IPTools\IP;
+use IPTools\Range;
 use Symfony\Component\HttpFoundation\Response;
 
 class Vpn
@@ -14,11 +16,17 @@ class Vpn
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // skip if not in production
-        if (! env('APP_IS_VPN')) {
-            return Redirect::route('error');
+        // either APP_IS_VPN must be set to true or the request ip must be in the APP_VPN_IP cidr range
+        if (env('APP_IS_VPN')) {
+            return $next($request);
+        }
+        if (env('APP_VPN_IP') && array_key_exists("HTTP_X_REAL_IP", $_SERVER)) {
+            $app_vpn_ip = Range::parse(env('APP_VPN_IP'));
+            if ($app_vpn_ip->contains(new IP($_SERVER['HTTP_X_REAL_IP']))) {
+                return $next($request);
+            }
         }
 
-        return $next($request);
+        return Redirect::route('error');
     }
 }
--- app/Http/Middleware/VpnOrPerson.php
+++ app/Http/Middleware/VpnOrPerson.php
@@ -6,6 +6,8 @@ use App\Models\Person;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use IPTools\IP;
+use IPTools\Range;
 use Symfony\Component\HttpFoundation\Response;
 
 class VpnOrPerson
@@ -15,11 +17,21 @@ class VpnOrPerson
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // skip if not in production
-        if (! env('APP_IS_VPN') && ($request->session()->missing('authToken') ||
-            ! Person::where('auth_token', $request->session()->get('authToken'))->count())
-        ) {
-            return Redirect::route('error');
+        // either APP_IS_VPN must be set to true or the request ip must be in the APP_VPN_IP cidr range
+        if (env('APP_IS_VPN')) {
+            return $next($request);
+        }
+        if (env('APP_VPN_IP') && array_key_exists("HTTP_X_REAL_IP", $_SERVER)) {
+            $app_vpn_ip = Range::parse(env('APP_VPN_IP'));
+            if ($app_vpn_ip->contains(new IP($_SERVER['HTTP_X_REAL_IP']))) {
+                return $next($request);
+            }
+        }
+        if (!$request->session()->missing('authToken')) {
+            return $next($request);
+        }
+        if (Person::where('auth_token', $request->session()->get('authToken'))->count()) {
+            return $next($request);
         }
 
         return $next($request);
