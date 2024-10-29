05a4d79ba5c3a8befa5424c7643bec31537699b6
Jan Pihl <jan96.pi@gmail.com>
2024-10-29 17:29:02 +0100

    Fix migration issue when using sqlite

--- database/migrations/2024_10_27_140636_update_icon_in_articles_table.php
+++ database/migrations/2024_10_27_140636_update_icon_in_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,11 +13,40 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('articles', function (Blueprint $table) {
-            $table->string('icon')->nullable(false)->change();
-        });
+        $driver = DB::getConfig('driver');
 
-        DB::statement('ALTER TABLE articles DROP CONSTRAINT IF EXISTS articles_icon_check;');
+        // sqlite doesn't support the 'alter table drop constraint' command
+        // the workaround needs to be used if sqlite is selected as database driver
+        if ($driver != 'sqlite') {
+            Schema::table('articles', function (Blueprint $table) {
+                $table->string('icon')->nullable(false)->change();
+            });
+            DB::statement('ALTER TABLE articles DROP CONSTRAINT IF EXISTS articles_icon_check;');
+        } else {
+            Schema::create('articles2', function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+                $table->string('name', 50)->nullable(false);
+                $table->string('icon')->nullable(false);
+                $table->integer('show_order')->nullable(false);
+                $table->integer('max_order_amount')->nullable(false)->default(1);
+            });
+
+            foreach (Article::all() as $article) {
+                DB::table('articles2')->insert(array(
+                    'id' => $article->id,
+                    'created_at' => $article->created_at,
+                    'updated_at' => $article->updated_at,
+                    'name' => $article->name,
+                    'show_order' => $article->show_order,
+                    'max_order_amount' => $article->max_order_amount
+                ));
+            }
+
+            Schema::dropIfExists('articles');
+
+            Schema::rename('articles2', 'articles');
+        }
     }
 
     /**
