b2def1113dc2b59b334ec8f8c609a8fbff5ed3fb
Nikidaa <nikita.diser@googlemail.com>
2025-10-14 07:22:59 +0200

    nothing really done

---
+++ resources/js/router.js
@@ -0,0 +1,13 @@
+import {createMemoryHistory, createRouter} from "vue-router";
+
+
+const routes = [
+
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+
+export default router
