e37310594e9a84512a640e71a5c5dd043b95809f
Simon Ostendorf <github@simon-ostendorf.de>
2025-10-17 17:47:56 +0200

    feat: deployment (#226)

---
+++ deploy/terraform/s3.tf
@@ -0,0 +1,6 @@
+# bucket will be created by portals and reused, currently no s3 setup needed
+
+# resource "minio_s3_bucket" "this" {
+#   bucket        = var.s3_bucket
+#   acl           = "private"
+# }
