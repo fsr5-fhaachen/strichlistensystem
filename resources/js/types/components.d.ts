52069a2459d5987b3b933dac1959032fc8868099
nd9824s <nd9824s@ad.fh-aachen.de>
2025-10-24 08:28:08 +0200

    .

--- resources/js/types/components.d.ts
+++ resources/js/types/components.d.ts
@@ -10,10 +10,12 @@ declare module 'vue' {
     AppButton: typeof import('./../components/AppButton.vue')['default']
     ArticleCard: typeof import('./../components/ArticleCard.vue')['default']
     LayoutContainer: typeof import('./../components/LayoutContainer.vue')['default']
+    NewPinField: typeof import('./../components/NewPinField.vue')['default']
     PersonAvatar: typeof import('./../components/PersonAvatar.vue')['default']
     PersonBadge: typeof import('./../components/PersonBadge.vue')['default']
     PersonCard: typeof import('./../components/PersonCard.vue')['default']
     PinField: typeof import('./../components/PinField.vue')['default']
+    PinInput: typeof import('./../components/PinInput.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
